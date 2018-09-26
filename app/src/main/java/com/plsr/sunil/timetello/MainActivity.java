842c9746035481a2028de656b7accac8641fc2ae
sunil <plsr.sunil@gmail.com>
2018-09-26 00:13:45 -0500

    calling ScreenReceiver service with a background service

--- app/src/main/java/com/plsr/sunil/timetello/MainActivity.java
+++ app/src/main/java/com/plsr/sunil/timetello/MainActivity.java
@@ -1,25 +1,41 @@
 package com.plsr.sunil.timetello;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent mServiceIntent;
+    private WakeyService mWakeyService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mWakeyService = new WakeyService(this);
+        mServiceIntent = new Intent(this, mWakeyService.getClass());
+        if (!isMyServiceRunning(mWakeyService.getClass())) {
+            startService(mServiceIntent);
+        }
+    }
 
-        // INITIALIZE RECEIVER
-        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        BroadcastReceiver mReceiver = new ScreenReceiver();
-        registerReceiver(mReceiver, filter);
-
-
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.i ("isMyServiceRunning?", true+"");
+                return true;
+            }
+        }
+        Log.i ("isMyServiceRunning?", false+"");
+        return false;
     }
 }
